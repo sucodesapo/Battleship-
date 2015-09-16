9068b6a73a83916941b47d6c393d8ae08008e6e8
Alexandre Lima <ale.basket@yahoo.com.br>
2015-09-16 09:40:17 -0300

    2

--- Start.py
+++ Start.py
@@ -8,3 +8,13 @@ Created on Wed Sep 16 08:50:46 2015
 #criando a base para o brinquedo
 
 base = []
+
+'''
+com o looping, eu vou adicionar itens à lista e fica assim:
+[ base[0] == ['O', 'O', 'O', 'O', 'O']... base[9] == ['O', 'O', 'O', 'O', 'O'] ] 
+'''
+
+for i in range(10):
+    while i < 10:
+        base.append(["O"]*10)
+        break
