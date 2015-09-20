96f540de99334f21cc4fd0195d85d3a5cb55a596
Alexandre Lima <ale.basket@yahoo.com.br>
2015-09-20 15:53:00 -0300

    5

--- Start.py
+++ Start.py
@@ -5,6 +5,9 @@ Created on Wed Sep 16 08:50:46 2015
 @author: Alexandre Lima
 """
 
+from random import randint 
+
+
 #criando a base para o brinquedo
 
 base = []
@@ -23,7 +26,29 @@ for i in range(10):
 
 def coluna(base):
     for t in base:
+        # tira as virgulas e deixa o jogo mais bonito
         print ('  '.join(t))
 coluna(base)
+
+# selecionar um ponto aleatório para esconder o barco
+
+def random_row(base):
+    return randint(0,9)
+    
+
+def random_colum(base):
+    return randint(0,9)
+    
+ship_row = random_row(base)
+ship_colum = random_colum(base)
+
+# perguntar ao user onde está o barco
+
+guess_row = int(input("Diga uma linha: "))
+guess_col = int(input("Diga uma coluna: "))
+
+
+
+
 
 
